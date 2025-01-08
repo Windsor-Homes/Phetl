486069220b25a06d3ae6504225e55321527088d3
LijahIde <lijahide196@gmail.com>
2025-01-08 13:38:52 -0500

    made base abstract extractor

---
+++ src/Extractors/Extractor.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Windsor\Phetl\Extractors;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Enumerable;
+use Windsor\Phetl\Concerns\HasLifecycleHooks;
+
+abstract class Extractor
+{
+    use HasLifecycleHooks;
+
+    abstract public function extract(): Enumerable;
+
+
+    public function __invoke(): Enumerable
+    {
+        $this->runHooks('before-extraction', $this);
+
+        $data = $this->extract();
+
+        $this->runHooks('after-extraction', $this, $data);
+
+        return $data;
+    }
+
+    /**
+     * Set a callable that will be executed after the data is extracted from the response.
+     *
+     * The callable will have access to the Extractor instance and the Extracted data.
+     *
+     * @param callable(Extractor, Collection) $callback
+     * @return static
+     */
+    public function afterExtraction(callable $callback): static
+    {
+        $this->addHook('after-extraction', $callback);
+
+        return $this;
+    }
+
+    /**
+     * Set a callable that will be executed before the data is extracted.
+     *
+     * The callable will have access to the Extractor instance
+     *
+     * @param callable(Extractor) $callback
+     * @return static
+     */
+    public function beforeExtraction(callable $callback): static
+    {
+        $this->addHook('before-extraction', $callback);
+
+        return $this;
+    }
+}
