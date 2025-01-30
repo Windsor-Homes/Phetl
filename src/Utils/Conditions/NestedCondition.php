9a816e3fb1c8b64728542e9eb920da270ef3fb28
LijahIde <lijahide196@gmail.com>
2025-01-30 09:38:36 -0500

    removed method docblocks
    this class does not forward these method calls to the builder class

--- src/Utils/Conditions/NestedCondition.php
+++ src/Utils/Conditions/NestedCondition.php
@@ -4,35 +4,6 @@ namespace Windsor\Phetl\Utils\Conditions;
 
 /**
  * Condition that groups multiple conditions together
- *
- * @method where($field, $operator, $value, $conjunction = 'and', $negate = false)
- * @method orWhere($field, $operator, $value)
- * @method whereNot($field, $operator, $value)
- * @method orWhereNot($field, $operator, $value)
- * @method whereIn($field, $values)
- * @method orWhereIn($field, $values)
- * @method whereNotIn($field, $values)
- * @method orWhereNotIn($field, $values)
- * @method whereBetween($field, $values)
- * @method orWhereBetween($field, $values)
- * @method whereNotBetween($field, $values)
- * @method orWhereNotBetween($field, $values)
- * @method whereNull($field)
- * @method orWhereNull($field)
- * @method whereNotNull($field)
- * @method orWhereNotNull($field)
- * @method whereColumn($field, $operator, $column_value, $conjunction = 'and', $negate = false)
- * @method orWhereColumn($field, $operator, $column_value)
- * @method whereColumnNot($field, $operator, $column_value)
- * @method orWhereColumnNot($field, $operator, $column_value)
- * @method whereColumnIn($field, $column_value)
- * @method orWhereColumnIn($field, $column_value)
- * @method whereColumnNotIn($field, $column_value)
- * @method orWhereColumnNotIn($field, $column_value)
- * @method whereColumnBetween($field, $column_value)
- * @method orWhereColumnBetween($field, $column_value)
- * @method whereColumnNotBetween($field, $column_value)
- * @method orWhereColumnNotBetween($field, $column_value)
  */
 class NestedCondition extends Condition
 {
