7d343fdd030d116790d44114bbb42d0d136a3ae9
LijahIde <lijahide196@gmail.com>
2025-02-05 13:34:27 -0500

    added hooks to the base filter

--- src/Transformers/Filters/BaseFilter.php
+++ src/Transformers/Filters/BaseFilter.php
@@ -3,14 +3,35 @@
 namespace Windsor\Phetl\Transformers\Filters;
 
 use Illuminate\Support\Enumerable;
+use Windsor\Phetl\Concerns\HasLifecycleHooks;
 use Windsor\Phetl\Contracts\Transformer;
 
 abstract class BaseFilter implements Transformer
 {
+    use HasLifecycleHooks;
+
     abstract protected function filter(Enumerable $dataset): Enumerable;
 
     public function transform(Enumerable $dataset): Enumerable
     {
-        return $this->filter($dataset);
+        $this->runHooks('before-filter', $this, $dataset);
+
+        $dataset = $this->filter($dataset);
+
+        $this->runHooks('after-filter', $this, $dataset);
+
+        return $dataset;
+    }
+
+    public function beforeFilter(callable $callback): self
+    {
+        $this->addHook('before-filter', $callback);
+        return $this;
+    }
+
+    public function afterFilter(callable $callback): self
+    {
+        $this->addHook('after-filter', $callback);
+        return $this;
     }
 }
